82e0f0ecb9a2717f5c14517b85b688549cf63a08
lipeng <hlp1019878@163.com>
2020-01-11 23:16:20 +0800

    vue原理

--- kvue.js
+++ kvue.js
@@ -17,10 +17,18 @@
 function defineReactive(obj, key, val) {
   // 递归
   observe(val)
+
+  // 创建一个Dep和当前key一一对应
+  const dep = new Dep()
+
+
   // 对传入obj进行访问拦截
   Object.defineProperty(obj, key, {
     get() {
       console.log('get '+ key);
+      // 依赖收集在这里
+      Dep.target && dep.addDep(Dep.target)
+
       return val
     },
     set(newVal){
@@ -29,6 +37,10 @@ function defineReactive(obj, key, val) {
         // 如果传入的newVal依然是obj,需要做相应化处理
         observe(newVal)
         val = newVal
+
+        // 
+        // watchers.forEach(w => w.update())
+        dep.notify()
       }
     }
   })
@@ -72,6 +84,9 @@ class KVue {
 
     // 响应化处理
     observe(this.$data)
+
+    // 创建编译器实例
+    new Compiler(options.el, this)
   }
 }
 
@@ -93,10 +108,45 @@ class Observer {
     })
   }
 
-  // 数组数据的响应化
-  // walk(obj) {
-  //   Object.keys(obj).forEach(key => {
-  //     defineReactive(obj, key, obj[key])
-  //   })
-  // }
+  // 数组数据的响应化， 待补充
+  
+}
+
+// 观察者:  保存更新函数，值发生变化调用更新函数
+// const watchers = []
+class Watcher {
+  constructor(vm, key, updateFn) {
+    this.vm = vm
+
+    this.key = key
+
+    this.updateFn = updateFn
+
+    // watchers.push(this)
+
+    // Dep.target静态属性上设置为当前Watcher实例
+    Dep.target = this
+    this.vm[this.key] //读取触发了getter
+    Dep.target = null
+
+  }
+
+  update() {
+    this.updateFn.call(this.vm,this.vm[this.key])
+  }
+}
+
+// Dep: 依赖， 管理某个key相关的所有Watcher实例
+class Dep {
+  constructor() {
+    this.deps = []
+  }
+
+  addDep(dep) {
+    this.deps.push(dep)
+  }
+
+  notify() {
+    this.deps.forEach(dep=> dep.update())   //set里执行的操作
+  }
 }
